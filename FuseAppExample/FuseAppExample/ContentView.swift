c1c51c04fb64c2c07e2b661df226735a7f41756a
Nathan Peles <nathan@publift.com>
2025-12-10 14:54:13 +1100

    Fuse App Example

--- FuseAppExample/FuseAppExample/ContentView.swift
+++ FuseAppExample/FuseAppExample/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
                 Text("Banner Ad")
                     .font(.headline)
                 
+                // Display a banner ad.
                 FuseAdViewRepresentable(code: "banner")
                 
                 Divider()
@@ -27,12 +28,17 @@ struct ContentView: View {
                 Text("Banner Ad With Custom Parameters")
                     .font(.headline)
                 
+                // Send some extra info when we request the banner ad.
                 FuseAdViewRepresentable(
                     code: "banner",
                     params: FuseAdViewParams(
                         customTargeting: ["sport": "basketball", "size": "small"],
                         contentUrls: ["https://www.publift.com/about"]
-                    )
+                    ),
+                    onEvent: { adView, adViewEvent in
+                        // You can listen to banner ad events.
+                        print("Fuse Event: \(adViewEvent)")
+                    }
                 )
                 
                 Divider()
@@ -40,6 +46,7 @@ struct ContentView: View {
                 Text("Banner Ad With Custom Placeholder Image")
                     .font(.headline)
                 
+                // Customise the appearance of the banner ad whilst loading and if there's an error.
                 FuseAdViewRepresentable(
                     code: "example_no_fill_banner",
                     loadingBehaviour: .background(image: UIImage(named: "Placeholder")),
@@ -51,6 +58,7 @@ struct ContentView: View {
                 Text("Native Ad")
                     .font(.headline)
                 
+                // Display a native ad instead of a banner ad.
                 FuseAdViewRepresentable(
                     code: "native",
                     loadingBehaviour: .progressBar,
